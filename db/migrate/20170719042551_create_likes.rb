4bf4f50492d37cbac91fb74e0953d372c4c0ac8b
KimSuHwanMoo <f887kr@naver.com>
2017-07-19 14:17:24 +0900

    test

--- db/migrate/20170719042551_create_likes.rb
+++ db/migrate/20170719042551_create_likes.rb
@@ -1,7 +1,9 @@
 class CreateLikes < ActiveRecord::Migration[5.0]
   def change
     create_table :likes do |t|
-
+      # index 까지 추가된다
+      t.belongs_to :user
+      t.belongs_to :post
 
       t.timestamps
     end
